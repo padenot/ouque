23197adb4eefaa3d3f01b20f062e4ff7f318a922
Paul Adenot <paul@paul.cx>
2020-04-10 16:06:09 +0000

    .

--- blogupdate.sh
+++ blogupdate.sh
@@ -7,5 +7,5 @@ fi
 
 cd blog
 git pull origin master
-hugo -d /var/www/blog --cleanDestinationDir
-hugo -d /var/www/homepage/blogpreview --cleanDestinationDir --buildDrafts  -b "https://paul.cx/blogpreview/"
+hugo -d /var/www/blog -b https://blog.paul.cx --cleanDestinationDir
+hugo -d /var/www/homepage/blogpreview --cleanDestinationDir --buildDrafts -F  -b "https://paul.cx/blogpreview/"
